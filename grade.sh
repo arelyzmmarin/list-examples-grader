d9210d06ad14dcfa4709c2e1e3580fd2a0a66c31
arelyzmmarin <146775481+arelyzmmarin@users.noreply.github.com>
2023-11-27 13:00:11 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,6 +8,40 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [ -f `student-submission/ListExamples` ]
+then
+    echo "found file"
+else 
+    echo "did not find file"
+fi
+cp student-submission/ListExamples.java grading-area
+#cd grading-area
+javac ListExamples.java TestListExamples.java
+if [[$? -ne 0]]
+then    
+    echo "Compile Error"
+    exit
+else
+    echo "Compile Successful"
+fi
+passed = 0
+failed = 0
+for test-file in grading-area
+do
+    result = `java ListExamples`
+    expect = `java TestListExamples`
+    if [[$expect == $result]]
+    then
+        passed = $(($passed+1))
+    else
+        failed = $(($failed+1))
+    fi
+done
+    echo "$1: Test results: $passed passed, $failed failed" > result.txt
+
+
+
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
